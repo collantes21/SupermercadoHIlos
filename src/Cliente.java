ebb146ee285a6826d043f933e1ef2b1e79be5ea2
DAM <alvaritocollantes21@hotmail.com>
2023-10-09 13:07:39 +0200

    Modificacion nombre a cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,15 +1,31 @@
 public class Cliente {
 
     int numHilo=0;
-    public class Saludo extends Thread {
+    public class Saludo {
 
-        int numHilo=0;
+        private String nombre;
+        private int[] carroCompra;
 
-
-        public Saludo(int numHilo) {
-            this.numHilo = numHilo;
+        public Saludo(String nombre, int[] carroCompra) {
+            this.nombre = nombre;
+            this.carroCompra = carroCompra;
         }
 
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public int[] getCarroCompra() {
+            return carroCompra;
+        }
+
+        public void setCarroCompra(int[] carroCompra) {
+            this.carroCompra = carroCompra;
+        }
 
         @Override
         public void run() {
